e6cb175234ce6c6fafce51fdbfaa868fc2218480
Fatih Sarikaya <fatihsarikaya4@hotmail.com>
2022-10-28 13:48:32 +0300

    Create Insertion Sort Projesi.py
    for patika

---
+++ Insertion Sort Projesi.py
@@ -0,0 +1,49 @@
+#Insertion Sort Projesi
+
+'''
+[22,27,16,2,18,6] -> Insertion Sort
+
+Yukarı verilen dizinin sort türüne göre aşamalarını yazınız.
+Big-O gösterimini yazınız.
+Time Complexity: Average case: Aradığımız sayının ortada olması,Worst case: Aradığımız sayının sonda olması, Best case: Aradığımız sayının dizinin en başında olması.
+Dizi sıralandıktan sonra 18 sayısı hangi case kapsamına girer? Yazınız.
+
+
+[7,3,5,8,2,9,4,15,6] dizisinin Insertion Sort'a göre ilk 4 adımını yazınız.
+'''
+
+def insertionSort(arr):
+ 
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+ 
+        key = arr[i]
+ 
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i-1
+        while j >=0 and key < arr[j] :
+                arr[j+1] = arr[j]
+                j -= 1
+        arr[j+1] = key
+ 
+ 
+#sorting the array [22, 27, 16, 2, 18, 6] using insertionSort
+arr = [22, 27, 16, 2, 18, 6]
+#arr = [7,3,5,8,2,9,4,15,6]
+insertionSort(arr)
+lst = [] #empty list to store sorted elements
+print("Sorted array is : ")
+for i in range(len(arr)):
+    lst.append(arr[i])     #appending the elements in sorted order
+print(lst)
+
+# Big-O Representation O(n^2);
+
+# Time Complexity: Best Case = [2,6,16,18,22,27]; Worst Case = [27,22,18,16,6,2];
+
+# What case does the number 18 fall into after the array is sorted?
+#  -Since 18 is in the middle after sorting, it is covered by the average case.
+
+
